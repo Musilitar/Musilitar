cb395c23b67af64579c71f8c6c3d77764a11d15f
Lode Lesage <musilitar@gmail.com>
2014-11-29 16:46:09 +0100

    API main update

--- api/main.py
+++ api/main.py
@@ -6,16 +6,17 @@ connection = Connection("localhost", 27017)
 db = connection.tweets
 collection = db.user
 
-@app.route("/t", methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
+    return "I am Musilitar. I am too big for the box."
+
+
+@app.route("/t", methods=["GET"])
+def tweets():
     tweets = []
     for tweet in collection.find():
         tweets.append(tweet)
     return jsonify(text=tweets[0]["text"])
-
-@app.route("/", methods=["GET"])
-def index():
-    return "I am Musilitar. I am too big for the box."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)

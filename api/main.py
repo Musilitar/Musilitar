38c36afd0f1bde7abbdf53024faa0685a46b11f1
Lode Lesage <musilitar@gmail.com>
2014-11-29 15:05:00 +0100

    Delete crossdomain usage

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from pymongo import Connection
-from api.crossdomain import crossdomain
 
 app = Flask(__name__)
 connection = Connection("localhost", 27017)
@@ -8,7 +7,6 @@ db = connection.tweets
 collection = db.user
 
 @app.route("/t", methods=["GET"])
-@crossdomain(origin="*")
 def index():
     tweets = []
     for tweet in collection.find():

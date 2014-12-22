243999ce817dd4e69ea22b961f5453c77747f467
Lode Lesage <musilitar@gmail.com>
2014-12-22 15:59:00 +0100

    Delete crossdomain, added filtering of question / info with response and into database, implemented questions in API

--- api/main.py
+++ api/main.py
@@ -3,20 +3,42 @@ from pymongo import Connection
 
 app = Flask(__name__)
 connection = Connection("localhost", 27017)
-db = connection.tweets
-collection = db.user
+tweets = connection.tweets
+tUser = tweets.user
+tAQuestions = tweets.aquestions
+tUQuestions = tweets.uquestions
+tKInfo = tweets.kinfo
+tUInfo = tweets.uinfo
+
 
 @app.route("/", methods=["GET"])
-def index():
+def home():
     return "I am Musilitar. I am too big for the box."
 
 
 @app.route("/t", methods=["GET"])
 def tweets():
     tweets = []
-    for tweet in collection.find():
+    for tweet in tUser.find():
         tweets.append(tweet)
     return jsonify(text=tweets[0]["text"])
 
+
+@app.route("/uq", methods=["GET"])
+def unansweredMostRecent():
+    questions = []
+    for question in tUQuestions.find():
+        questions.append(question)
+    return jsonify(text=questions[0]["text"])
+
+@app.route("/uq/<int:index>", methods=["GET"])
+def unansweredByID(index):
+    questions = []
+    for question in tUQuestions.find():
+        questions.append(question)
+    return jsonify(text=questions[index]["text"])
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run()
+    #app.run(host="0.0.0.0", port=80, debug=True)
--- core/main.py
+++ core/main.py
@@ -8,32 +8,61 @@ TOKEN = "856006422-RjbHJ7x0CMyWPEelGQiedyT2JecOo0mJBskan068"
 TOKEN_SECRET = "xrLuD2xZVwVChXLr5VWLRBL10Ad6Urow7ks8SW16n7ewt"
 
 twitter = Twython(APP_KEY, APP_SECRET, TOKEN, TOKEN_SECRET)
+connection = Connection("localhost", 27017)
+tweets = connection.tweets
+tUser = tweets.user
+tAQuestions = tweets.aquestions
+tUQuestions = tweets.uquestions
+tKInfo = tweets.kinfo
+tUInfo = tweets.uinfo
 
 
 class Streamer(TwythonStreamer):
     def on_success(self, data):
-        if 'id_str' in data:
-            id_str = data['id_str']
-            text = "@" + data['user']['screen_name'] + " Right back at you!"
-            twitter.update_status(status=text, in_reply_to_status_id_str=id_str)
-            print(data['text'].encode('utf-8'))
+        if "id_str" in data:
+            if data["user"]["screen_name"] != "Musilitar":
+                id_str = data["id_str"]
+                response = "@" + data["user"]["screen_name"] + " " + dismantle(data)
+                twitter.update_status(status=response, in_reply_to_status_id_str=id_str)
+                print(data["text"].encode("utf-8"))
 
     def on_error(self, status_code, data):
         print(status_code)
+        self.disconnect()
 
 
 def main():
-    connection = Connection("localhost", 27017)
-    db = connection.tweets
-    collection = db.user
-
-    tweet = twitter.get_home_timeline()[0]
-    collection.insert(tweet)
+    # tweet = twitter.get_home_timeline()[0]
+    #tweets.insert(tweet)
+    listen()
 
 
-def echo():
+def listen():
     streamer = Streamer(APP_KEY, APP_SECRET, TOKEN, TOKEN_SECRET)
     streamer.user()
+
+
+def dismantle(tweet):
+    inAnswered = tAQuestions.find_one({"text": tweet["text"]})
+    inUnanswered = tUQuestions.find_one({"text": tweet["text"]})
+    inKnown = tKInfo.find_one({"text": tweet["text"]})
+    inUnknown = tUInfo.find_one({"text": tweet["text"]})
+    if "?" in tweet["text"]:
+        if inAnswered is None and inUnanswered is None:
+            tUQuestions.insert(tweet)
+            return "Question, new"
+        elif inAnswered is not None:
+            return "Question, answered"
+        else:
+            return "Question, unanswered"
+    else:
+        if inKnown is None and inUnknown is None:
+            tUInfo.insert(tweet)
+            return "Info, new"
+        elif inKnown is not None:
+            return "Info, known"
+        else:
+            return "Info, unknown"
 
 
 if __name__ == "__main__":
